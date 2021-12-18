0546ea81cebc7c681d1ec7b0c869a09f3161692e
AipBorges <mpbds@uic.es>
2021-12-18 16:21:20 +0100

    added instructions for all exercises

--- src/components/BoardPanel.vue
+++ src/components/BoardPanel.vue
@@ -2,24 +2,54 @@
   <section class="board-panel">
     <div class="board-panel-header">
       <div class="board-panel-header__methods">
-        <button class="board-panel-header__button" @click="onClickMethod('push')">PUSH</button>
-        <button class="board-panel-header__button" @click="onClickMethod('filter')">FILTER</button>
-        <button class="board-panel-header__button" @click="onClickMethod('map')">MAP</button>
+        <button
+          class="board-panel-header__button"
+          @click="onClickMethod('push')"
+        >
+          PUSH
+        </button>
+        <button
+          class="board-panel-header__button"
+          @click="onClickMethod('filter')"
+        >
+          FILTER
+        </button>
+        <button
+          class="board-panel-header__button"
+          @click="onClickMethod('map')"
+        >
+          MAP
+        </button>
       </div>
       <div class="board-panel-header__elements">
-        <button class="board-panel-header__button" @click="onClickElement('carrot')">CARROT</button>
-        <button class="board-panel-header__button" @click="onClickElement('hay')">HAY</button>
-        <button class="board-panel-header__button" @click="onClickElement('horseshoe')">
+        <button
+          class="board-panel-header__button"
+          @click="onClickElement('carrot')"
+        >
+          CARROT
+        </button>
+        <button
+          class="board-panel-header__button"
+          @click="onClickElement('hay')"
+        >
+          HAY
+        </button>
+        <button
+          class="board-panel-header__button"
+          @click="onClickElement('horseshoe')"
+        >
           HORSESHOE
         </button>
       </div>
     </div>
-    <p class="board-panel__user-input">array.map(cosas)</p>
+    <p class="board-panel__user-input">{{ this.method }} {{ this.element }}</p>
 
     <p>La pista</p>
 
     <div>
-      <button class="board-panel-footer__button" @click="onClickClear">CLEAR</button>
+      <button class="board-panel-footer__button" @click="onClickClear">
+        CLEAR
+      </button>
       <button
         class="board-panel-footer__button board-panel-footer__button--next"
         @click="onClickNext"
@@ -118,3 +148,4 @@ export default defineComponent({
   }
 }
 </style>
+
