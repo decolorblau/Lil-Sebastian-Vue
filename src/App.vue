324a800094bdd76a84cb3c8f2681ba9e49460f3c
Yeray Sarmiento <y@MacBook-Pro-de-Yeray.local>
2021-12-18 14:13:48 +0100

    Fixed prop in Array component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <header></header>
+    <header>
+      <img class="logo" alt="Lil Sebastian Logo" src="@/assets/logo.png" width="100" height="100" />
+      <h1 class="title">Lil Sebastian</h1>
+    </header>
     <div class="container">
       <router-view />
     </div>
@@ -17,7 +20,6 @@
 #app {
   height: 100vh;
   width: 100vw;
-  background-color: orange;
 }
 
 header {
@@ -25,17 +27,19 @@ header {
   width: 100%;
   background-color: blue;
   position: fixed;
+  display: flex;
 }
 
 footer {
   position: fixed;
   bottom: 0;
-  height: 100px;
+  height: 50px;
   width: 100%;
   background-color: yellow;
 }
 
 .container {
-  padding: 100px 0;
+  background-color: orange;
+  height: 100%;
 }
 </style>
