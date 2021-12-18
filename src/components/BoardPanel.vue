221d3b4406098cda44cbb97a27724d99df61bd82
Claudia Font <claudiafontmarques@gmail.com>
2021-12-18 15:14:22 +0100

    added buttons initial styles

--- src/components/BoardPanel.vue
+++ src/components/BoardPanel.vue
@@ -2,15 +2,32 @@
   <section class="board-panel">
     <header class="board-panel-header">
       <div class="board-panel-header__methods">
-        <button class="board-panel-header__button" @click="onClickMethod('push')">PUSH</button>
-        <button class="board-panel-header__button" @click="onClickMethod('filter')">FILTER</button>
-        <button class="board-panel-header__button" @click="onClickMethod('map')">MAP</button>
+        <button class="board-panel-header__button-method" @click="onClickMethod('filter')">
+          FILTER
+        </button>
+        <button class="board-panel-header__button-method" @click="onClickMethod('push')">
+          FIND
+        </button>
+        <button class="board-panel-header__button-method" @click="onClickMethod('map')">MAP</button>
+        <button class="board-panel-header__button-method" @click="onClickMethod('push')">
+          POP
+        </button>
+        <button class="board-panel-header__button-method" @click="onClickMethod('push')">
+          PUSH
+        </button>
       </div>
       <div class="board-panel-header__elements">
-        <button class="board-panel-header__button" @click="onClickElement('carrot')">CARROT</button>
-        <button class="board-panel-header__button" @click="onClickElement('hay')">HAY</button>
+        <button class="board-panel-header__button" @click="onClickElement('carrot')">
+          <img class="board-panel-header__image" src="../assets/carrot.png" alt="carrot" />
+        </button>
+        <button class="board-panel-header__button" @click="onClickElement('hay')">
+          <img class="board-panel-header__image" src="../assets/hay.png" alt="hay" />
+        </button>
         <button class="board-panel-header__button" @click="onClickElement('horseshoe')">
-          HORSESHOE
+          <img class="board-panel-header__image" src="../assets/horseshoe.png" alt="hay" />
+        </button>
+        <button class="board-panel-header__button" @click="onClickElement('hair-comb')">
+          <img class="board-panel-header__image" src="../assets/hair-comb.png" alt="hair-comb" />
         </button>
       </div>
     </header>
@@ -79,4 +96,27 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.board-panel-header {
+  display: flex;
+  flex-direction: column;
+  &__button-method {
+    width: 100px;
+    height: 40px;
+    background-color: white;
+    border: none;
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    border-radius: 50px;
+    cursor: pointer;
+  }
+  &__button {
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+  }
+  &__image {
+    width: 40px;
+    height: 40px;
+  }
+}
+</style>
